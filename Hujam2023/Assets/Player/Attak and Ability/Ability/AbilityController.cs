ba3b817f4ca582c0cea577132529b30944c5864f
Gurkan.gur <gurkan.gur@tigahealth.com>
2023-12-20 17:45:53 +0300

    update

--- Hujam2023/Assets/Player/Attak and Ability/Ability/AbilityController.cs
+++ Hujam2023/Assets/Player/Attak and Ability/Ability/AbilityController.cs
@@ -4,5 +4,76 @@ using UnityEngine;
 
 public class AbilityController : MonoBehaviour
 {
-    public AbilityTypeEnum AbilityType; 
+    [SerializeField] private AbilityValue UFO;
+
+    public AbilityTypeEnum AbilityType;
+
+    private bool attack = true;
+    private float attackCastTime = 0;
+
+    private Rigidbody2D rb2d;
+    private PlayerMovment MovCS;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        MovCS = GetComponent<PlayerMovment>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (attack && !MovCS.Stuned() && Input.GetKeyDown(KeyCode.X))
+        {
+            AttackForMe();
+        }
+        else
+        {
+            if (attackCastTime <= 0) attack = true;
+            else attackCastTime -= Time.deltaTime;
+        }
+    }
+
+    private void AttackForMe()
+    {
+        MovCS.DontMove = true;
+        attack = false;
+        SellectType();
+    }
+
+    private void SellectType()
+    {
+        switch (AbilityType)
+        {
+            case AbilityTypeEnum.UFO:
+                uFO();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void WaitAttak(float waitTime)
+    {
+        attackCastTime += waitTime;
+    }
+
+    private void uFO()
+    {
+        rb2d.velocity = Vector2.zero;
+        rb2d.gravityScale = 0f;
+
+        attackCastTime = this.UFO.abilityCastTime;
+
+        GameObject UFO = Instantiate(this.UFO.ability, transform.position, transform.rotation);
+    }
 }
